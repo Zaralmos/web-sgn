c7684388c798f7a889076ec817c556af89d67a95
Sergei Solovev <sergey.soloviev@inbox.ru>
2019-03-13 14:27:54 +0300

    basic modules added

---
+++ WebSgn/web-sgn-ng-client/src/app/departments/departments.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DepartmentsRoutingModule } from './departments-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DepartmentsRoutingModule
+  ]
+})
+export class DepartmentsModule { }
---
+++ WebSgn/web-sgn-ng-client/src/app/enrollee/enrollee.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EnrolleeRoutingModule } from './enrollee-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EnrolleeRoutingModule
+  ]
+})
+export class EnrolleeModule { }
